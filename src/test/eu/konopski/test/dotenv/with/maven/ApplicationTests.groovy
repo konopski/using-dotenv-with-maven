93a35d83808f2d152e4029793c1e27fdcbe5c2dd
Lukasz Konopski <konopski@users.noreply.github.com>
2023-02-05 17:26:30 +0100

    Update ApplicationTests.groovy

--- src/test/eu/konopski/test/dotenv/with/maven/ApplicationTests.groovy
+++ src/test/eu/konopski/test/dotenv/with/maven/ApplicationTests.groovy
@@ -1,6 +1,7 @@
 package eu.konopski.test.dotenv.with.maven
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
